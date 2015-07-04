8ff5246c78ad6bd99df4dd4f042ce087aa8fdb27
Takuma YOSHITANI <takuma.7@gmail.com>
2015-07-04 17:24:41 +0900

    set up

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'lib')));
 
 app.use('/', routes);
 app.use('/users', users);
